104e0f28408a820264129b05ea05352720f279ab
bilal-sagir <sagir.bilal@icloud.com>
2021-11-29 04:50:59 +0300

    nsobject insert

--- ToDoApp2/DataStructure.swift
+++ ToDoApp2/DataStructure.swift
@@ -1,8 +1,30 @@
 import Foundation
 
 
-struct Item
+class Item: NSObject, NSCoding
 {
     var name: String!
     var status: Bool!
+    
+    override init(){
+    }
+    
+    init(name: String, status: Bool){
+        self.name = name
+        self.status = status
+    }
+    
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(self.name, forKey: "name")
+        coder.encode(self.status.string, forKey: "status")
+    }
+    
+    required convenience init?(coder: NSCoder)
+    {
+        let name = coder.decodeObject(forKey: "name") as! String
+        let status = coder.decodeObject(forKey: "status") as! String
+        
+        self.init(name: name, status: status.bool)
+    }
 }
